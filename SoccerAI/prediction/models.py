16088092271e7c9fe37aeba643a4d87d6066d1d0
Ferrari-Boy <107806722+Ferrari-Boy@users.noreply.github.com>
2022-12-03 20:56:02 +0300

    modified:   SoccerAI/db.sqlite3 	modified:   SoccerAI/prediction/__pycache__/admin.cpython-311.pyc 	modified:   SoccerAI/prediction/__pycache__/models.cpython-311.pyc 	modified:   SoccerAI/prediction/__pycache__/urls.cpython-311.pyc 	modified:   SoccerAI/prediction/admin.py 	new file:   SoccerAI/prediction/migrations/0001_initial.py 	new file:   SoccerAI/prediction/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   SoccerAI/prediction/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   SoccerAI/prediction/models.py

--- SoccerAI/prediction/models.py
+++ SoccerAI/prediction/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Matches(models.Model):
     Date = models.DateField('Дата')
-    Country = models.CharField('Страна')
-    Tournament = models.CharField('Турнир')
+    Country = models.CharField('Страна', max_length=50)
+    Tournament = models.CharField('Турнир', max_length=50)
     Time = models.TimeField('Время')
-    Teams = models.CharField('Команды')
+    Teams = models.CharField('Команды', max_length=100)
     PreviewLink = models.URLField('Ссылка на матч')
-    HomeTeam = models.CharField('Хозяева')
-    AwayTeam = models.CharField('Гости')
+    HomeTeam = models.CharField('Хозяева', max_length=50)
+    AwayTeam = models.CharField('Гости', max_length=50)
     HomeHref = models.URLField('Ссылка на Хозяев')
     AwayHref = models.URLField('Ссылка на Гостей')
     HTRating = models.FloatField('Рейтинг Хозяев')
@@ -27,8 +27,8 @@ class Matches(models.Model):
     AwayGoalDif = models.FloatField('Разница з/п голов Гостей')
     HTMatches = models.IntegerField('Кол-во матчей Хозяев')
     ATMatches = models.IntegerField('Кол-во матчей Гостей')
-    HTLastMatches = models.CharField('Последние матчи Хозяев')
-    ATLastMatches = models.CharField('Последние матчи Гостей')
+    HTLastMatches = models.CharField('Последние матчи Хозяев', max_length=50)
+    ATLastMatches = models.CharField('Последние матчи Гостей', max_length=50)
     HTPoints = models.FloatField('Очки Хозяев за последние матчи')
     ATPoints = models.FloatField('Очки Гостей за последние матчи')
     AvgCH = models.FloatField('1')
@@ -44,11 +44,15 @@ class Matches(models.Model):
     HA_Prob = models.FloatField('12 Вероятность')
     AD_Prob = models.FloatField('X2 Вероятность')
     WhoScoredDifference = models.IntegerField('Разница мячей в прогнозе WhoScored')
-    WhoScoredPrediction = models.CharField('Пронгоз WhoScored')
-    Prediction = models.CharField('Прогноз')
-    ExpressPrediction = models.CharField('Пронгоз для Экспресс-пула')
-    Result = models.CharField('Результат матча')
+    WhoScoredPrediction = models.CharField('Пронгоз WhoScored', max_length=20)
+    Prediction = models.CharField('Прогноз', max_length=20)
+    ExpressPrediction = models.CharField('Пронгоз для Экспресс-пула', max_length=20)
+    Result = models.CharField('Результат матча', max_length=20)
     
     def __str__(self):
         return self.Teams
+    
+    class Meta:
+        verbose_name = 'Матч'
+        verbose_name_plural = 'Матчи'
     
