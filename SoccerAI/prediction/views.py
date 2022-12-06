f3fe6fd472fe42172c44d1d5c05a359f4e2ae367
Ferrari-Boy <ferrariboy4k@gmail.com>
2022-12-07 02:30:59 +0300

    modified:   SoccerAI/SoccerAI/__pycache__/settings.cpython-311.pyc 	modified:   SoccerAI/SoccerAI/settings.py 	modified:   SoccerAI/prediction/__pycache__/views.cpython-311.pyc 	modified:   SoccerAI/prediction/static/prediction/css/main.css 	modified:   SoccerAI/prediction/templates/prediction/expresspool.html 	modified:   SoccerAI/prediction/templates/prediction/prediction.html 	modified:   SoccerAI/prediction/views.py

--- SoccerAI/prediction/views.py
+++ SoccerAI/prediction/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Matches
 
 def prediction(request):
-    return render(request, 'prediction/prediction.html')
+    matches = Matches.objects.filter(~Q(Prediction="No"))
+        
+    return render(request, 'prediction/prediction.html', {'matches' : matches})
 
 def expresspool(request):
-    return render(request, 'prediction/expresspool.html')
+    matches = Matches.objects.filter(~Q(ExpressPrediction="No"))
+    return render(request, 'prediction/expresspool.html', {'matches' : matches})
